34b4ed6f7dc11562a90ef955ec6ba6df7d2d2a2e
Stas Denisov <nxrighthere@gmail.com>
2019-12-03 17:49:13 +0500

    Migrate to MIT

--- Source/Managed/NanoSockets.cs
+++ Source/Managed/NanoSockets.cs
@@ -1,18 +1,25 @@
-/****************************** Module Header ******************************\
-* Module Name:  NanoSockets.cs
-* Project:      NanoSockets
-* Copyright (c) 2019 Stanislav Denisov
-*
-* Lightweight UDP sockets abstraction for rapid implementation of message-oriented protocols
-*
-* This source is subject to the Microsoft Public License.
-* See https://opensource.org/licenses/MS-PL
-* All other rights reserved.
-*
-* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
-* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-\***************************************************************************/
+/*
+ *  Lightweight UDP sockets abstraction for rapid implementation of message-oriented protocols
+ *  Copyright (c) 2019 Stanislav Denisov
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
 
 using System;
 using System.Runtime.InteropServices;
